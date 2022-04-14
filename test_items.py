09b0a575caba47979d35a1205f214a6a425d6daf
ValarValar <dan4eg98@mail.ru>
2022-04-14 16:24:07 +0300

    Task is done

---
+++ test_items.py
@@ -0,0 +1,12 @@
+import pytest
+from selenium import webdriver
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+from selenium.webdriver.common.by import By
+
+class TestItems():
+
+    @pytest.mark.smoke
+    def test_item_has_add_to_basket_button(self, browser):
+        browser.get(link)
+        browser.find_element(by=By.CSS_SELECTOR, value="#add_to_basket_form button.btn-add-to-basket")
