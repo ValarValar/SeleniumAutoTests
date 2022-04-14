4ead7de23757f581f9f0f1d52ae20f3612a27c07
ValarValar <dan4eg98@mail.ru>
2022-04-14 16:57:05 +0300

    Assert added (task req)

--- test_items.py
+++ test_items.py
@@ -9,4 +9,5 @@ class TestItems():
     @pytest.mark.smoke
     def test_item_has_add_to_basket_button(self, browser):
         browser.get(link)
-        browser.find_element(by=By.CSS_SELECTOR, value="#add_to_basket_form button.btn-add-to-basket")
+        button = browser.find_elements(by=By.CSS_SELECTOR, value="#add_to_basket_form button.btn-add-to-basket")
+        assert button, "Кнопка не была найдена!"
